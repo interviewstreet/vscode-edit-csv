8ea55c75c2647cea45e036d27763ea2e1b90bc02
deepak-goyal-hackerrank <deepak.goyal@hackerrank.com>
2026-06-17 16:21:35 +0530

    Fix Vitest Browser and esbuild advisories (#9)

--- csvEditorHtml/test/vitest.config.ts
+++ csvEditorHtml/test/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
 
 export default defineConfig({
 
@@ -7,65 +8,11 @@ export default defineConfig({
       enabled: true,
       headless: true,
       screenshotFailures: false,
-      name: 'chromium',
-      provider: 'playwright',
-      testerScripts: [
-        {
-          src: './node_modules/dayjs/dayjs.min.js',
-          type: 'text/javascript',
-        },
-        {
-          src: './node_modules/dayjs/plugin/customParseFormat.js',
-          type: 'text/javascript',
-        },
-        {
-          src: './thirdParty/big.js/big.min.js',
-          type: 'text/javascript',
-        },
-        {
-          src: './thirdParty/toFormat/toFormat.min.js',
-          type: 'text/javascript',
-        },
-        {
-          src: './thirdParty/regression/regression.min.js', //our regression uses big.js
-          type: 'text/javascript',
-        },
-        {
-          src: './thirdParty/papaparse/papaparse.umd.js',
-          type: 'text/javascript',
-        },
-        {
-          src: './csvEditorHtml/util.ts',
-          type: 'text/javascript',
-        },
-        {
-          src: './csvEditorHtml/io.ts',
-          type: 'text/javascript',
-        },
-        {
-          src: './csvEditorHtml/autoFill.ts',
-          type: 'text/javascript',
-        },
-        //could also be moved to init...
-        {
-          content: `
-          window.numbersStyleEnRadio = {
-            checked: true
-          }
-          //add toFormat to big numbers
-          toFormat(Big)
-          //for custom formatted dates
-          dayjs.extend(dayjs_plugin_customParseFormat);
-          
-          `,
-          type: 'text/javascript',
-        },
-        {
-          src: './csvEditorHtml/test/init.ts',
-          type: 'text/javascript',
-        },
-
+      provider: playwright(),
+      instances: [
+        { browser: 'chromium' },
       ],
+      testerHtmlPath: 'csvEditorHtml/test/tester.html',
     },
     include: ['csvEditorHtml/test/**/*.test.ts'],
   },
